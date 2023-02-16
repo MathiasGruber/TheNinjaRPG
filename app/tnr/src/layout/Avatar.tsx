c29097b2a4c424373a140fa9e3ea65b26612961a
Mathias Gruber <utgdk@leo-pharma.com>
2023-02-16 18:12:43 +0000

    AvatarPriority

--- app/tnr/src/layout/Avatar.tsx
+++ app/tnr/src/layout/Avatar.tsx
@@ -6,6 +6,7 @@ interface AvatarImageProps {
   href?: string | null;
   alt: string;
   size: number;
+  priority?: boolean;
 }
 
 const AvatarImage: React.FC<AvatarImageProps> = (props) => {
@@ -14,11 +15,12 @@ const AvatarImage: React.FC<AvatarImageProps> = (props) => {
   } else {
     return (
       <Image
-        className="m-auto w-5/6 rounded-2xl border-2 border-black"
+        className="m-auto w-5/6 rounded-2xl border-2 border-black hover:border-amber-500 hover:opacity-80"
         src={props.href}
         alt={props.alt + " AvatarImage"}
         width={props.size}
         height={props.size}
+        priority={props.priority}
       />
     );
   }
