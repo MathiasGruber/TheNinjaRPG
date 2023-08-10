412bfb5c326e92932c44ddea4cdc7c8b2a40204f
Mathias Gruber <nano.mathias@gmail.com>
2023-08-10 16:19:02 +0000

    :sparkles: Entire battle log

--- app/src/layout/CombatHistory.tsx
+++ app/src/layout/CombatHistory.tsx
@@ -1,45 +1,109 @@
 import React, { useState, useEffect } from "react";
 import Loader from "./Loader";
 import { api } from "../utils/api";
+import { getBattleRound } from "../libs/combat/actions";
+import { groupBy } from "../utils/grouping";
+import { useInfinitePagination } from "../libs/pagination";
 import type { ActionEffect } from "../libs/combat/types";
+import type { ReturnedBattle } from "../libs/combat/types";
 
 interface CombatHistoryProps {
-  battleId: string;
-  battleVersion: number;
+  battle: ReturnedBattle;
 }
 
 const CombatHistory: React.FC<CombatHistoryProps> = (props) => {
-  // Destructure
-  const { battleId, battleVersion } = props;
-
   // State
-  const [version, setVersion] = useState<number>(battleVersion);
+  const { battle } = props;
+  const [lastElement, setLastElement] = useState<HTMLDivElement | null>(null);
+  const [version, setVersion] = useState<number>(battle.version);
 
   // From database
-  const { data, isFetching: isFetchingHistory } = api.combat.getBattleEntry.useQuery(
-    { battleId: battleId, version: version },
-    { staleTime: Infinity, keepPreviousData: true }
-  );
-  const effects = data && (data.appliedEffects as ActionEffect[]);
+  const { data, fetchNextPage, hasNextPage, isFetching } =
+    api.combat.getBattleEntries.useInfiniteQuery(
+      { battleId: battle.id, limit: 10, refreshKey: version },
+      {
+        enabled: battle.id !== undefined,
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+        keepPreviousData: true,
+        staleTime: Infinity,
+      }
+    );
+  useInfinitePagination({ fetchNextPage, hasNextPage, lastElement });
+
+  const allEntries = data?.pages
+    .map((page) => page.data)
+    .flat()
+    .map((entry) => {
+      const { round } = getBattleRound(battle, entry.createdAt.getTime());
+      return { ...entry, round };
+    });
+  const groups = allEntries && groupBy(allEntries, "round");
+
+  // const { data, isFetching: isFetchingHistory } = api.combat.getBattleEntry.useQuery(
+  //   { battleId: battleId, version: version },
+  //   { staleTime: Infinity, keepPreviousData: true }
+  // );
+  // const effects = allEntries && (allEntries.appliedEffects as ActionEffect[]);
 
   // Create an array from zero to maxVersion
-  const versionArray = Array.from(Array(battleVersion).keys());
-  let lastHidden = false;
+  // const versionArray = Array.from(Array(battleVersion).keys());
+  // let lastHidden = false;
 
   // Update version whenever battleVersion changes
   useEffect(() => {
-    setVersion(battleVersion);
-  }, [battleVersion]);
+    setVersion(battle.version);
+  }, [battle.version]);
+
+  // Create the history
+  const history: React.ReactNode[] = [];
+  const lastVersion = allEntries && allEntries.at(-1)?.battleVersion;
+  groups?.forEach((entries, round) => {
+    history.push(
+      <li key={round} className=" ml-4">
+        <div className="absolute w-3 h-3 rounded-full mt-1.5 -left-1.5 border border-gray-900 bg-gray-700"></div>
+        <time className="mb-1 text-sm font-normal leading-none text-gray-900">
+          Round {round}
+        </time>
+        {entries.map((entry) => {
+          const effects = entry.appliedEffects as ActionEffect[];
+          return (
+            <div
+              key={`v-${entry.battleVersion}`}
+              ref={entry.battleVersion === lastVersion ? setLastElement : null}
+              className="mb-4 text-base font-normal text-gray-500"
+            >
+              #{entry.battleVersion}: {entry.description}
+              {effects?.map((effect, i) => {
+                const color =
+                  effect.color === "red"
+                    ? "text-red-500"
+                    : effect.color === "blue"
+                    ? "text-blue-500"
+                    : "text-green-500";
+                return (
+                  <p key={i} className={color}>
+                    - {effect.txt}
+                  </p>
+                );
+              })}
+            </div>
+          );
+        })}
+      </li>
+    );
+  });
 
   // Show component
   return (
-    <div className="flex flex-col items-center justify-center border-b-2 border-l-2 border-r-2 bg-slate-100">
-      {isFetchingHistory && (
-        <div className="absolute">
+    <div className="relative flex flex-col border-b-2 border-l-2 border-r-2 bg-slate-100 max-h-48 overflow-auto">
+      {isFetching && (
+        <div className="absolute right-2 top-2">
           <Loader />
         </div>
       )}
-      <div className="px-2 pt-2">
+      <ol className="relative border-l border-gray-700 w-full">{history}</ol>
+
+      {/* <div className="px-2 pt-2">
         {data && (
           <>
             <i>{data.description}</i>
@@ -91,6 +155,7 @@ const CombatHistory: React.FC<CombatHistoryProps> = (props) => {
           })}
         </ul>
       </div>
+     */}
     </div>
   );
 };
