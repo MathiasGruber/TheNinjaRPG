77bd87c4366c4942a35b9e9045aa4279456e793b
Mathias Gruber <nano.mathias@gmail.com>
2023-09-06 18:52:16 +0000

    :loud_sound: Highlight.io logging init

--- app/src/pages/api/trpc/[trpc].ts
+++ app/src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,12 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { createTRPCContext } from "../../../server/api/trpc";
 import { appRouter } from "../../../server/api/root";
-import { Handlers } from "@highlight-run/node";
+import { Handlers, H } from "@highlight-run/node";
 
+// Highlight.io init
+H.init({ projectID: process.env.NEXT_PUBLIC_HIGHLIGHT_IO_PROJECT_ID });
+
+// Configure Vercel
 export const config = {
   // runtime: "edge",
   regions: ["iad1"],
