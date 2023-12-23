36cacbf7192820b785e1775bd24a1259907f4a23
Mathias Gruber <nano.mathias@gmail.com>
2023-12-23 17:45:19 +0000

    :children_crossing: Add level to AI overall statistics

--- app/src/server/api/routers/data.ts
+++ app/src/server/api/routers/data.ts
@@ -36,7 +36,7 @@ export const dataRouter = createTRPCRouter({
     .query(async ({ ctx, input }) => {
       const usage = await ctx.drizzle
         .select({
-          name: userData.username,
+          name: sql`CONCAT(${userData.username}, ' - lvl', ${userData.level})`,
           battleWon: dataBattleAction.battleWon,
           count: sql<number>`COUNT(${dataBattleAction.id})`.mapWith(Number),
         })
