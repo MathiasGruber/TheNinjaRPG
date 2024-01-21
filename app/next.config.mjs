8b61c27d8d897abcaa0b1ec714cd85095c78aae3
Mathias Gruber <nano.mathias@gmail.com>
2024-01-21 05:20:31 +0000

    Allow plausible

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -65,7 +65,7 @@ export default withAxiom(withBundleAnalyzer(config));
 // https://securityheaders.com
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google-analytics.com *.googletagmanager.com *.doubleclick.net *.clerk.accounts.dev *.vercel.live  *.paypal.com *.tiny.cloud *.theninja-rpg.com *.opendns.com *.highlight.io;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google-analytics.com *.googletagmanager.com *.doubleclick.net *.clerk.accounts.dev *.vercel.live  *.paypal.com *.tiny.cloud *.theninja-rpg.com *.opendns.com *.highlight.io *.plausible.io;
   child-src 'self' *.doubleclick.net *.paypal.com ghbtns.com *.youtube.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com *.tiny.cloud;
   img-src * blob: data:;
