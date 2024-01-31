295949cc79cebfb3d34ff4d5ef09b1a71e6e6198
Mathias Gruber <nano.mathias@gmail.com>
2024-01-31 18:05:01 +0000

    📈 Change ga4 code

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -29,18 +29,22 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         },
       }}
     >
-      {cookieConsent === "true" && (
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${env.NEXT_PUBLIC_MEASUREMENT_ID}');
-        `}
-        </Script>
-      )}
       <Header />
+      {cookieConsent === "true" && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+          />
+          <Script
+            id="google-tag-manager"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${env.NEXT_PUBLIC_MEASUREMENT_ID}', {page_path: window.location.pathname});`,
+            }}
+          />
+        </>
+      )}
       <ToastContainer />
       <CookieConsent cookieName="TNR-cookie-conscent">
         This website uses cookies to enhance the user experience. Please read our{" "}
