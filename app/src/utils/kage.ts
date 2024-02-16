9dc9d84be059adda193c42480614af042e753ae0
Mathias Gruber <nano.mathias@gmail.com>
2024-02-16 18:56:40 +0000

    ✨ Set different kage challenge costs for Alpha testing

--- app/src/utils/kage.ts
+++ app/src/utils/kage.ts
@@ -1,9 +1,9 @@
 import { hasRequiredRank } from "@/libs/train";
 import type { UserData } from "@/drizzle/schema";
 
-export const PRESTIGE_REQUIREMENT = 5;
-export const RANK_REQUIREMENT = "STUDENT";
-export const PRESTIGE_COST = 5;
+export const PRESTIGE_REQUIREMENT = 5; // Should be 30
+export const RANK_REQUIREMENT = "JONIN";
+export const PRESTIGE_COST = 0; // Should be 5
 
 export const canChallengeKage = (user: UserData) => {
   if (
