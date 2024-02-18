32ddeaf883d2ed7c48eb83e8429ad8ca306aac5d
Mathias Gruber <nano.mathias@gmail.com>
2024-02-18 17:38:33 +0000

    ✨ Set a prestige cost for challenging kage

--- app/src/utils/kage.ts
+++ app/src/utils/kage.ts
@@ -3,7 +3,7 @@ import type { UserData } from "@/drizzle/schema";
 
 export const PRESTIGE_REQUIREMENT = 5; // Should be 30
 export const RANK_REQUIREMENT = "JONIN";
-export const PRESTIGE_COST = 0; // Should be 5
+export const PRESTIGE_COST = 1; // Should be 5
 
 export const canChallengeKage = (user: UserData) => {
   if (
