0a45eb39d52a19d56beef4ae7c40653a93a687d8
Mathias Gruber <nano.mathias@gmail.com>
2024-04-19 08:40:21 +0000

    ✨ Move toasts to upper right

--- app/src/components/ui/toast.tsx
+++ app/src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed z-[100] flex max-h-screen w-full p-4 bottom-0 right-0 top-auto flex-col md:max-w-[420px]",
+      "fixed z-[100] flex max-h-screen w-full p-4 top-0 right-0 bottom-auto flex-col md:max-w-[420px]",
       className,
     )}
     {...props}
