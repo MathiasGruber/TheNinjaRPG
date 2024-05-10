871554b3f4155c0a1a76e09675a40ce0ebfb5430
Mathias Gruber <nano.mathias@gmail.com>
2024-05-10 18:04:43 +0000

    🐛 IAmHere for quests

--- app/src/layout/Combat.tsx
+++ app/src/layout/Combat.tsx
@@ -135,7 +135,7 @@ const Combat: React.FC<CombatProps> = (props) => {
     if (
       battle.current &&
       isInLobby &&
-      !["KAGE", "ARENA"].includes(battle.current.battleType)
+      !["KAGE", "ARENA", "QUEST"].includes(battle.current.battleType)
     ) {
       const user = battle.current.usersState.find((u) => u.userId === suid);
       if (user && !user.iAmHere) {
