57c0d12eeb1f9f7e50ced238b39a1bfc59621d86
Mathias Gruber <nano.mathias@gmail.com>
2024-06-18 07:24:00 +0000

    ⬆️ Bump next, trpc, react-query & fix drizzle-kit

--- app/drizzle.config.ts
+++ app/drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./drizzle/migrations",
   schema: "./drizzle/schema.ts",
-  driver: "mysql2",
+  dialect: "mysql",
   ...(process.env.DATABASE_URL
     ? { dbCredentials: { uri: process.env.DATABASE_URL } }
     : {}),
