767fb31e05207fcf5a4756dd2eab281dc16705df
Mathias Gruber <nano.mathias@gmail.com>
2024-09-26 06:45:14 +0000

    👔 Adjust bloodline roll chances

--- app/src/libs/bloodline.ts
+++ app/src/libs/bloodline.ts
@@ -2,20 +2,12 @@ export const ROLL_CHANCE = {
   ["H"]: 0.001,
   ["S"]: 0.005,
   ["A"]: 0.01,
-  ["B"]: 0.02,
-  ["C"]: 0.04,
-  ["D"]: 0.08,
+  ["B"]: 0.05,
+  ["C"]: 0.25,
+  ["D"]: 0.3,
 } as const;
 
-// export const BLOODLINE_COST = {
-//   ["S"]: 400,
-//   ["A"]: 200,
-//   ["B"]: 100,
-//   ["C"]: 50,
-//   ["D"]: 25,
-// } as const;
-
-// Alpha Pricing
+// Bloodline Pricing
 export const BLOODLINE_COST = {
   ["H"]: 999999,
   ["S"]: 999999,
