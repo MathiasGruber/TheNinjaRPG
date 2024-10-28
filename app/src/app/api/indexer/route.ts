cffa551a810f776caf534ae16cb9d5022a15fc27
Mathias Gruber <nano.mathias@gmail.com>
2024-10-28 13:10:07 +0000

    👔 No need for limit on this endpoint

--- app/src/app/api/indexer/route.ts
+++ app/src/app/api/indexer/route.ts
@@ -11,7 +11,7 @@ export async function GET() {
   // Check timer
   const frequency = 1;
   const response = await checkGameTimer(drizzleDB, frequency, "m", "indexer");
-  if (response) return response;
+  // if (response) return response;
 
   try {
     // Update timer
