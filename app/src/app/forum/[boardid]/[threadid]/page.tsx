f09cd5d3ca895648e22e51d7d10ef9fc07102214
Mathias Gruber <nano.mathias@gmail.com>
2024-12-05 06:28:28 +0000

    🔍️ Improve thread descriptions

--- app/src/app/forum/[boardid]/[threadid]/page.tsx
+++ app/src/app/forum/[boardid]/[threadid]/page.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { parseHtml } from "@/utils/parse";
+import Link from "next/link";
 import Loader from "@/layout/Loader";
 import Pagination from "@/layout/Pagination";
 import ContentBox from "@/layout/ContentBox";
@@ -71,9 +72,73 @@ export default function Thread({ params }: { params: { threadid: string } }) {
 
   return (
     <>
+      {!userData && (
+        <ContentBox title="Public Forum" back_href={"/forum/" + thread.boardId}>
+          <p>
+            Register an account to also dive into the world of{" "}
+            <strong>The Ninja RPG</strong> on our forum. This is the place to connect
+            with fellow players, share your strategies, ask questions, and explore the
+            rich lore of the ninja world. Whether you&apos;re here to discuss game
+            mechanics, brainstorm new tactics, or simply enjoy the camaraderie of our
+            passionate community, this thread is your gateway to valuable insights and
+            collaboration.
+          </p>
+          <p className="pt-2">
+            Have questions about missions, combat strategies, or character builds? Share
+            them here and get advice from experienced players. Looking to understand the
+            intricacies of ninja ranks, clan dynamics, or crafting systems? The threads
+            found on the forum is a treasure trove of tips and tricks that will help you
+            level up your gameplay and enhance your ninja journey.
+          </p>
+          <p className="pt-2">
+            Don&apos;t forget to check out the{" "}
+            <Link className="font-bold" href="/manual">
+              game manual
+            </Link>{" "}
+            for detailed guides and instructions on mastering every aspect of{" "}
+            <strong>The Ninja RPG</strong>. Stay connected with our vibrant{" "}
+            <Link className="font-bold" href="https://discord.gg/grPmTr4z9C">
+              Discord community
+            </Link>{" "}
+            for real-time discussions, announcements, and exclusive events. For the
+            latest updates and contributions, visit our{" "}
+            <Link
+              className="font-bold"
+              href="https://github.com/MathiasGruber/TheNinjaRPG/issues"
+            >
+              GitHub repository
+            </Link>
+            , where you can explore the game&apos;s development and even participate in
+            shaping its future. And, of course, make sure to explore other threads in
+            the{" "}
+            <Link className="font-bold" href="/forum">
+              forums
+            </Link>{" "}
+            to uncover even more tips, debates, and strategies.
+          </p>
+          <p className="pt-2">
+            This forum thread is part of the thriving community that makes{" "}
+            <strong>The Ninja RPG</strong> special. Whether you&apos;re a seasoned ninja
+            or just starting your journey, you&apos;ll find this thread to be a
+            welcoming and resourceful space. Share your ideas, learn from others, and
+            contribute to the growing knowledge base of our ninja world.
+          </p>
+          <p className="pt-2">
+            Ready to jump into the discussion? Join the conversation below and
+            let&apos;s keep building the ninja legacy together. Haven&apos;t started
+            your adventure yet?{" "}
+            <Link className="font-bold" href="/login">
+              sign up today
+            </Link>{" "}
+            at <strong>theninja-rpg.com</strong> and become part of the most immersive
+            ninja RPG experience online!
+          </p>
+        </ContentBox>
+      )}
       <ContentBox
         title="Forum"
-        back_href={"/forum/" + thread.boardId}
+        back_href={userData ? "/forum/" + thread.boardId : undefined}
+        initialBreak={userData ? false : true}
         subtitle={thread.title}
       >
         {allComments?.map((comment, i) => {
