1100e7bce05901b245ae7772bc3ab6d17846e6de
Mathias Gruber <nano.mathias@gmail.com>
2025-01-03 12:00:10 +0100

    Remove comment

--- app/src/libs/combat/actions.ts
+++ app/src/libs/combat/actions.ts
@@ -52,7 +52,7 @@ export const availableUserActions = (
   // Concatenate all actions
   let availableActions = [
     ...(basicMoves && !isStealth ? [basicActions.basicAttack] : []),
-    ...(basicMoves ? [basicActions.basicHeal] : []), // Basic heal allowed during stealth
+    ...(basicMoves ? [basicActions.basicHeal] : []),
     ...(!isImmobilized ? [basicActions.basicMove] : []),
     ...(basicMoves && !isStealth
       ? [basicActions.basicClear, basicActions.basicCleanse, basicActions.basicFlee]
