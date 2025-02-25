63bf556489ec5a02f61805d220b50212b94d4fc0
Phrosfire <65364337+Phrosfire@users.noreply.github.com>
2025-02-24 19:09:17 -0500

    change user.ryo to user.money
    Signed-off-by: Phrosfire <65364337+Phrosfire@users.noreply.github.com>

--- app/src/server/api/routers/home.ts
+++ app/src/server/api/routers/home.ts
@@ -95,7 +95,7 @@ export const homeRouter = createTRPCRouter({
       });
       if (!selectedHome) return errorResponse("Invalid home type");
 
-      if (user.ryo < selectedHome.cost) {
+      if (user.money < selectedHome.cost) {
         return errorResponse("Not enough ryo");
       }
 
@@ -114,7 +114,7 @@ export const homeRouter = createTRPCRouter({
 
       await ctx.drizzle.transaction(async (tx) => {
         await tx.update(userData)
-          .set({ ryo: user.ryo - selectedHome.cost })
+          .set({ ryo: user.money - selectedHome.cost })
           .where(eq(userData.userId, ctx.userId));
 
         if (currentHome) {
