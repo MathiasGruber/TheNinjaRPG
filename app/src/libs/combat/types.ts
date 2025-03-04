0aece3157408de3878d4e32e67be07d5207f1e5a
Phrosfire <65364337+Phrosfire@users.noreply.github.com>
2025-03-03 21:07:26 -0500

    Remove accidental =
    Signed-off-by: Phrosfire <65364337+Phrosfire@users.noreply.github.com>

--- app/src/libs/combat/types.ts
+++ app/src/libs/combat/types.ts
@@ -934,7 +934,7 @@ export type UserEffect = BattleEffect & {
 
 export type ActionEffect = {
   txt: string;
-  type EffectColor = | "red" | "green" | "blue" | "yellow" | "purple" | "orange" | "pink" | "gray";
+  type EffectColor: | "red" | "green" | "blue" | "yellow" | "purple" | "orange" | "pink" | "gray";
   types?: (GeneralType | StatType | ElementName | PoolType)[];
 };
 
