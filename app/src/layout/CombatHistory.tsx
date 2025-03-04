205b30508ba007efd5b6f47c46a89fd41e81548e
Phrosfire <65364337+Phrosfire@users.noreply.github.com>
2025-03-03 20:55:06 -0500

    Add additional color options for battle text
    Signed-off-by: Phrosfire <65364337+Phrosfire@users.noreply.github.com>

--- app/src/layout/CombatHistory.tsx
+++ app/src/layout/CombatHistory.tsx
@@ -86,11 +86,15 @@ const CombatHistory: React.FC<CombatHistoryProps> = (props) => {
                   : ""}
                 {effects?.map((effect, i) => {
                   const color =
-                    effect.color === "red"
-                      ? "text-red-500"
-                      : effect.color === "blue"
-                        ? "text-blue-500"
-                        : "text-green-500";
+                    effect.color === "red" ? "text-red-500" :
+                    effect.color === "blue" ? "text-blue-500" :
+                    effect.color === "green" ? "text-green-500" :
+                    effect.color === "yellow" ? "text-yellow-500" :
+                    effect.color === "purple" ? "text-purple-500" :
+                    effect.color === "orange" ? "text-orange-500" :
+                    effect.color === "pink" ? "text-pink-500" :
+                    effect.color === "gray" ? "text-gray-500" :
+                    "text-black";
                   const text = insertComponentsIntoText(effect.txt, {
                     Highest: (
                       <span key={`${round}-${i}-H`} className="text-stone-500">
