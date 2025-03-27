db8513041d83e11598fa1dd7d78575d3630d9c3f
Phrosfire <65364337+Phrosfire@users.noreply.github.com>
2025-03-27 06:58:22 -0400

    Use ranked loadout in ranked battles
    Signed-off-by: Phrosfire <65364337+Phrosfire@users.noreply.github.com>

--- app/src/server/api/routers/combat.ts
+++ app/src/server/api/routers/combat.ts
@@ -1038,11 +1038,19 @@ export const initiateBattle = async (
           where: (items) => and(gt(items.quantity, 0), ne(items.equipped, "NONE")),
           orderBy: (table, { desc }) => [desc(table.quantity)],
         },
-        jutsus: {
-          with: { jutsu: true },
-          where: (jutsus) => eq(jutsus.equipped, 1),
-          orderBy: (table, { desc }) => [desc(table.level)],
-        },
+        ...(battleType === "RANKED" ? {
+          rankedJutsus: {
+            with: { jutsu: true },
+            where: (jutsus) => eq(jutsus.equipped, 1),
+            orderBy: (table, { desc }) => [desc(table.level)],
+          }
+        } : {
+          jutsus: {
+            with: { jutsu: true },
+            where: (jutsus) => eq(jutsus.equipped, 1),
+            orderBy: (table, { desc }) => [desc(table.level)],
+          }
+        }),
         userQuests: {
           where: or(
             and(isNull(questHistory.endAt), eq(questHistory.completed, 0)),
