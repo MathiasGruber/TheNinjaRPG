2c93e9f05dc4da55c8e92cb6b66b85109c02c8cb
Mathias Gruber <nano.mathias@gmail.com>
2025-04-19 11:30:51 +0200

    :art: Coderabbit suggestion on useMemp overuse

--- app/src/libs/travel/globe.ts
+++ app/src/libs/travel/globe.ts
@@ -5,7 +5,7 @@ import { type GlobalMapData } from "./types";
  * Fetches the map data from the server.
  */
 export const fetchMap = async () => {
-  const response = await fetch(IMG_MAP_HEXASPHERE);
+  const response = await fetch(IMG_MAP_HEXASPHERE, { cache: "force-cache" });
   const hexasphere = await response.json().then((data) => data as GlobalMapData);
   return hexasphere;
 };
