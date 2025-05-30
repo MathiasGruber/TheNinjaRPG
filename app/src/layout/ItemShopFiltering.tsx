af3cdda0b29ced476fa173db44ba258a05f0896d
Phrosfire <65364337+Phrosfire@users.noreply.github.com>
2025-05-29 20:45:59 -0400

    Add missing import

--- app/src/layout/ItemShopFiltering.tsx
+++ app/src/layout/ItemShopFiltering.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
