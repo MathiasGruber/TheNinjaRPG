aeca0d14de7e1c8a0461b075b3c4b132b2743cfd
Mathias Gruber <nano.mathias@gmail.com>
2025-06-01 15:01:43 +0200

    :children_crossing: Ordering of menu in tutorial

--- app/src/layout/Tutorial.tsx
+++ app/src/layout/Tutorial.tsx
@@ -65,6 +65,14 @@ const TUTORIAL_STEPS: TutorialStepConfig[] = [
     requiresGameMenu: true,
   },
   {
+    title: "Users",
+    description:
+      "Search for users, view the strongest players or view the current staff and reach out to them.",
+    elementId: "tutorial-users",
+    page: "/profile",
+    requiresGameMenu: true,
+  },
+  {
     title: "Inbox",
     description: "Check your messages from other players and system notifications.",
     elementId: "tutorial-inbox",
@@ -72,10 +80,10 @@ const TUTORIAL_STEPS: TutorialStepConfig[] = [
     requiresGameMenu: true,
   },
   {
-    title: "Users",
+    title: "Jutsus",
     description:
-      "Search for users, view the strongest players or view the current staff and reach out to them.",
-    elementId: "tutorial-users",
+      "View all the jutsu that you have trained and equip them to use in battle.",
+    elementId: "tutorial-jutsus",
     page: "/profile",
     requiresGameMenu: true,
   },
@@ -96,10 +104,10 @@ const TUTORIAL_STEPS: TutorialStepConfig[] = [
     requiresGameMenu: true,
   },
   {
-    title: "Jutsus",
+    title: "Points",
     description:
-      "View all the jutsu that you have trained and equip them to use in battle.",
-    elementId: "tutorial-jutsus",
+      "You can support the game by purchasing reputation points or buying federal support here.",
+    elementId: "tutorial-points",
     page: "/profile",
     requiresGameMenu: true,
   },
@@ -111,10 +119,9 @@ const TUTORIAL_STEPS: TutorialStepConfig[] = [
     requiresGameMenu: true,
   },
   {
-    title: "Points",
-    description:
-      "You can support the game by purchasing reputation points or buying federal support here.",
-    elementId: "tutorial-points",
+    title: "Rules",
+    description: "View all the rules of the game here",
+    elementId: "tutorial-rules",
     page: "/profile",
     requiresGameMenu: true,
   },
